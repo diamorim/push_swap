fe9e973b0058076b8daea9ee9a27956d347c429d
diamorim <diamorim02@gmail.com>
2026-06-26 14:01:35 +0200

    just commenting some stuff out

--- ops_push.c
+++ ops_push.c
@@ -4,14 +4,14 @@ void	op_pa(t_prog_state *state)
 {
 	if (!state || !state->a || !state->b)
 		return ;
-	push(state->a, state->b);
-	record_op(state, OP_PA, "pa\n");
+	//push(state->a, state->b);
+	//record_op(state, OP_PA, "pa\n");
 }
 
 void	op_pb(t_prog_state *state)
 {
 	if (!state || !state->a || !state->b)
 		return ;
-	push(state->b, state->a);
-	record_op(state, OP_PB, "pb\n");
+	//push(state->b, state->a);
+	//record_op(state, OP_PB, "pb\n");
 }
