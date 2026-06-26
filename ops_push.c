71f05d0769f90d36a76f19709efab435442481ad
noah-42 <noahwork42@gmail.com>
2026-06-26 13:37:35 +0200

    examples of simplified setup for ops engine

---
+++ ops_push.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+void	op_pa(t_prog_state *state)
+{
+	if (!state || !state->a || !state->b)
+		return ;
+	push(state->a, state->b);
+	record_op(state, OP_PA, "pa\n");
+}
+
+void	op_pb(t_prog_state *state)
+{
+	if (!state || !state->a || !state->b)
+		return ;
+	push(state->b, state->a);
+	record_op(state, OP_PB, "pb\n");
+}
