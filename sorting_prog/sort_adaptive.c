3bb79efa6845cd71499f18f41312f31677bcb7b9
noah-42 <noahwork42@gmail.com>
2026-06-29 19:06:39 +0200

    sort_adaptive.c

--- sorting_prog/sort_adaptive.c
+++ sorting_prog/sort_adaptive.c
@@ -1,1 +1,16 @@
 #include "../push_swap.h"
+
+void	sort_adaptive(t_prog_state *state)
+{
+	double	disorder;
+
+	if (!state || !state->a)
+		return ;
+	disorder = state->disorder;
+	if (disorder < 0.2)
+		sort_simple(state);
+	else if (disorder >= 0.2 && disorder < 0.5)
+		sort_medium(state);
+	else
+		sort_complex(state);
+}
