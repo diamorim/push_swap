552e7d0ea57177823017ec435a5e2b3ed2f65fcf
noah-42 <noahwork42@gmail.com>
2026-06-29 13:50:37 +0200

    ops_a & ops_b

--- operations_prog/operations_a.c
+++ operations_prog/operations_a.c
@@ -13,41 +13,34 @@
 #include "../push_swap.h"
 
 void op_sa(t_prog_state *state) {
-  if (!state || !state->a || state->a->size < 2)
-    return;
-  swap(state->a);
-  record_ops(state, OP_SA);
-  ft_putstr_fd("sa\n", 1);
+	if (!state || !state->a || state->a->size < 2)
+		return;
+	  swap(state->a);
+	record_ops(state, OP_SA);
+	ft_putstr_fd("sa\n", 1);
 }
 
 void op_pa(t_prog_state *state) {
-  if (!state || !state->b)
-    return;
-  push(state->b, state->a);
-  record_ops(state, OP_PA);
-  ft_putstr_fd("pa\n", 1);
+	if (!state || !state->b)
+		return;
+	push(state->b, state->a);
+	record_ops(state, OP_PA);
+	ft_putstr_fd("pa\n", 1);
 }
 
 void op_ra(t_prog_state *state) {
-  if (!state || state->a)
-    return;
-  rotate(state->a);
-  record_ops(state, OP_RA);
-  ft_putstr_fd("ra\n", 1);
+	if (!state || state->a)
+		return;
+	rotate(state->a);
+	record_ops(state, OP_RA);
+	ft_putstr_fd("ra\n", 1);
 }
 
-void op_rra(t_prog_state *state) {
-  t_stack *stack_a;
-  if (!state || !state->a)
-    return;
-  reverse_rotate(state->a);
-  record_ops(state, OP_RRA);
-  ft_putstr_fd("rra\n", 1);
-}
 void op_rra(t_prog_state *state)
 {
-	if (!state->a || !state)
-		return ;
+	t_stack *stack_a;
+	if (!state || !state->a)
+		return;
 	reverse_rotate(state->a);
 	record_ops(state, OP_RRA);
 	ft_putstr_fd("rra\n", 1);
--- operations_prog/operations_b.c
+++ operations_prog/operations_b.c
@@ -1,42 +1,35 @@
 #include "../push_swap.h"
 
 void op_sb(t_prog_state *state) {
-  if (!state || !state->b || state->b->size < 2)
-    return;
-  swap(state->b);
-  record_ops(state, OP_SB);
-  ft_putstr_fd("sb\n", 1);
+	if (!state || !state->b || state->b->size < 2)
+		return;
+	swap(state->b);
+	record_ops(state, OP_SB);
+	ft_putstr_fd("sb\n", 1);
 }
 
 void op_pb(t_prog_state *state) {
-  if (!state->b || !state)
-    return;
-  push(state->a, state->b);
-  record_ops(state, OP_PB);
-  ft_putstr_fd("pb\n", 1);
+	if (!state || !state->b)
+		return;
+	push(state->a, state->b);
+	record_ops(state, OP_PB);
+	ft_putstr_fd("pb\n", 1);
 }
 
 void op_rb(t_prog_state *state) {
-  if (!state || !state->b)
-    return;
-  rotate(state->b);
-  record_ops(state, OP_RB);
-  ft_putstr_fd("rb\n", 1);
+	if (!state || !state->b)
+		return;
+	rotate(state->b);
+	record_ops(state, OP_RB);
+	ft_putstr_fd("rb\n", 1);
 }
 
-void op_rrb(t_prog_state *state) {
-  t_stack *stack_a;
-  if (!state || !state->b)
-    return;
-  reverse_rotate(state->b);
-  record_ops(state, OP_RRB);
-  ft_putstr_fd("rrb\n", 1);
-}
 void op_rrb(t_prog_state *state)
 {
-	if (!state->b || !state)
-		return ;
+  t_stack *stack_a;
+	if (!state || !state->b)
+		return;
 	reverse_rotate(state->b);
 	record_ops(state, OP_RRB);
-	ft_putstr_fd("rra\n", 1);
+	ft_putstr_fd("rrb\n", 1);
 }
