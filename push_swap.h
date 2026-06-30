cd59caed7f1b3c9806a84acccb6df9ed0717e914
noah-42 <noahwork42@gmail.com>
2026-06-30 14:35:22 +0200

    refactored smart_rotate into two functions -- easier to read and maintain -- updated comments as well -- push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -131,14 +131,13 @@ void			dispatch_algo_strategy(t_prog_state *state);
 int				is_sorted_asc(t_stack *s);
 
 void			sort_simple(t_prog_state *state);
-int				handle_small_sort(t_prog_state *state);
-void			sort_3(t_prog_state *state);
 
 void			sort_medium(t_prog_state *state);
 void			sort_complex(t_prog_state *state);
 
 
 //	___ sort_utils.c __
+void			sort_3(t_prog_state *state);
 int				handle_small_sort(t_prog_state *state);
 void			smart_rotate(t_prog_state *state, t_stack *s, int pos);
 
